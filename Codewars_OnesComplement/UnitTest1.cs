4cbe3381655005b9db75d79f00d83ac2f306ffb8
sqz777 <sqz777isme@gmail.com>
2017-12-28 20:45:22 +0800

    pass test case : input 1 and input 11

--- Codewars_OnesComplement/UnitTest1.cs
+++ Codewars_OnesComplement/UnitTest1.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Codewars_OnesComplement
@@ -9,21 +12,33 @@ namespace Codewars_OnesComplement
         [TestMethod]
         public void Input_StringEmpty_Should_Be_StringEmpty()
         {
-           Assert.AreEqual(string.Empty,Kata.OnesComplement(string.Empty));
+            Assert.AreEqual(string.Empty, Kata.OnesComplement(string.Empty));
         }
 
         [TestMethod]
         public void Input_1_Should_Be_0()
         {
-            Assert.AreEqual(0,Kata.OnesComplement("1"));
+            Assert.AreEqual("0", Kata.OnesComplement("1"));
+        }
+
+        [TestMethod]
+        public void Input_11_Should_Be_11()
+        {
+            Assert.AreEqual("00", Kata.OnesComplement("11"));
         }
     }
 
     public class Kata
     {
-        public static string OnesComplement(string empty)
+        public static string OnesComplement(string n)
         {
-            return string.Empty;
+            if (string.IsNullOrEmpty(n))
+                return string.Empty;
+            for (var i = 0; i < n.ToCharArray().Length; i++)
+            {
+                n.ToCharArray()[i] = '0';
+            }
+            return new string(n.ToCharArray());
         }
     }
 }
